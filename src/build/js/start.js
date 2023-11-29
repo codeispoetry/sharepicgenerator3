3594303c61a4265f6d4ab80e00d7866f856db2d1
Thomas Rose <thomas.rose@ionos.com>
2023-11-29 12:40:41 +0100

    fix

--- src/build/js/start.js
+++ src/build/js/start.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-undef, no-unused-vars */
-
-let api, sg, cockpit, select, undo, pixabay, uploader, component, rte, quill, logger, ui
+/* eslint-disable no-undef, no-unused-vars, no-var */
+var api, sg, cockpit, select, undo, pixabay, uploader, component, rte, quill, logger, ui
 
 window.onload = function () {
   api = new API()
