61f18a6f1fa7cad0984cfbf56371b7b3b2f4ff22
Thomas Rose <thomas.rose@ionos.com>
2024-01-23 17:31:04 +0100

    changes

--- src/Views/Cockpit/Sharepic.php
+++ src/Views/Cockpit/Sharepic.php
@@ -11,7 +11,7 @@
         
             
             <div class="brands">
-                <h4><?php  echo _('Presets');?>:</h4>
+                <small><?php echo _('Size presets');?>:</small>
                 <button data-sizepreset="1280:1280" title="<?php echo _('square 1:1'); ?>">
                     <img src="assets/icons/square1to1.svg">
                 </button>
@@ -27,40 +27,42 @@
                 <button data-sizepreset="1200:627" title="X">
                     <img src="assets/icons/brands/linkedin.svg">
                 </button>
+                
             </div>
     </section>
     
-    <section style="margin-top:4em">
-        <h3><?php echo _("Background image"); ?></h3>
+    <section style="margin-top:6em;padding-top: 1em; border-top: 1px solid white;">
+        <h3><?php echo _("Set background image"); ?></h3>
         <label style="display:flex; margin-top:20px;">
             <input type="text" style="width:100%;" name="pixabay_q" id="pixabay_q" placeholder="<?php  echo _('search query');?>">
             <button data-click="pixabay.search" style="padding:2px;"><img src="assets/icons/search.svg"></button>
         </label>
         
 
-        <label style="display:flex; align-items: center;padding:3px;margin-top:2em; width: 160px;" class="file-upload">
+        <label style="display:flex; align-items: center;padding:3px;margin-top:1em; width: 160px;" class="file-upload">
             <img src="assets/icons/upload.svg"> <?php  echo _('upload image');?>
             <input type="file" name="upload" id="upload">
         </label>
     </section>
 
-    <section style="margin-top:4em">
-        <label class="horizontal" style="margin:0">
-            <h4><?php  echo _('Background color');?></h4>
-            <input type="color" value="#ffffff" class="" id="background_color">
-        </label>
+    <section style="margin-top:3em">
+        <h3><?php echo _("Image preferences"); ?></h3>
         <label class="horizontal" style="margin-top:0">
-            <h4><?php  echo _('Background size');?></h4>
+            <h4><?php  echo _('Size');?></h4>
             <input type="range" min="10" max="300" value="100" class="slider" id="background_size">
         </label>
         <label style="display:flex;margin-top: 0">
             <button class="link" data-click="sg.reset_background">
-                <?php echo _("Fit background automatically"); ?>
+                <?php echo _("Fit automatically"); ?>
             </button>   
         </label>
         <label>
             <input type="checkbox" id="drag_background">
             <?php echo _("Make background draggable"); ?>
+        </label>
+        <label class="horizontal" style="margin:0">
+            <h4><?php  echo _('Color');?></h4>
+            <input type="color" value="#ffffff" class="" id="background_color">
         </label>
     </section>
     
