82c9bd119edf83fbbd0852aa6fd4949cd978c87f
Thomas Rose <thomas.rose@ionos.com>
2024-02-05 13:13:40 +0100

    re-organize menu

--- src/Views/Menu.php
+++ src/Views/Menu.php
@@ -4,7 +4,14 @@
         <div class="dropdown-content">
              <a href="index.php" class="menu-link">
                 <?php  echo _('Home');?>
-			</a>           
+			</a>   
+            <button data-click="api.save">
+                <?php  echo _('Save');?>
+            </button>
+            <button data-click="api.create">
+                <?php  echo _('Download');?>
+            </button>
+
             <div class="divider"></div>
             <button data-lang="de">
                 <?php echo _( 'German' ); ?>
@@ -40,49 +47,35 @@
 
     <div class="dropdown" id="menu_edit">
         <span><?php echo _( 'Edit' ); ?></span>
-        <div class="dropdown-content">   
-            <button data-click="api.save">
-                <?php  echo _('Save');?>
-            </button>
-            <button data-click="api.create">
-                <?php  echo _('Download');?>
-            </button>
+        <div class="dropdown-content adder">   
+           
             <button data-click="undo.undo" id="undo">
                 <?php echo _( 'Undo' ); ?>
             </button>
-        </div>
-    </div>
+            <div class="divider"></div>
 
-    <div class="dropdown" id="menu_add">
-        <span><?php echo _( 'Add' ); ?></span>
-        <div class="dropdown-content adder">
-            <button data-item="eyecatcher"><?php  echo _('Eyecatcher');?></button>
-            <button data-item="addpicture"><?php  echo _('Picture');?></button>
-
-        </div>
-    </div>
-
-    <div class="dropdown" id="menu_templates">
-        <span><?php echo _( 'Templates' ); ?></span>
-        <div class="dropdown-content">
-            <?php
-                $last = 'users/' . $this->user->get_username() . '/workspace/sharepic.html';
-                if (file_exists($last)) {
-                    echo '<button data-load="' . $last . '">' . _('latest') . '</button>';
-                }
-                if( 'auto' !== $this->user->get_role() ){
-                    foreach( $templates as $path => $label ) {
-                        printf( '<button data-load="templates/%s/start.html">%s</button>', $path, $label );
-                    }
-                }
-            ?>
+            <button data-item="eyecatcher"><?php  echo _('Add sticker');?></button>
+            <button data-item="addpicture"><?php  echo _('Add picture');?></button>
         </div>
     </div>
 
     <div class="dropdown" id="menu_mysharepics">
-        <span><?php echo _( 'My sharepics' ); ?></span>
+        <span><?php echo _( 'My templates' ); ?></span>
         <div class="dropdown-content" id="my-sharepics">
             <?php
+                $last = 'users/' . $this->user->get_username() . '/workspace/sharepic.html';
+                if (file_exists($last)) {
+                    echo '<button data-load="' . $last . '">' . _('latest') . '</button>';
+                    echo '<div class="divider"></div>';
+                }
+               
+                if( 'auto' !== $this->user->get_role() ){
+                    foreach( $templates as $path => $label ) {
+                        printf( '<button data-load="templates/%s/start.html">%s</button>', $path, $label );
+                    }
+                    echo '<div class="divider"></div>';
+                }
+
                $savings = $this->user->get_savings();
                foreach( $savings as $dir => $name ){
                     echo '<div class="dropdown-item-double">';
