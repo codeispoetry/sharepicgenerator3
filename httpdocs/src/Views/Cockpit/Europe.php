5d6e4ebbf4c0a41250a4307c1805b7cbf53b8657
Thomas Rose <thomas.rose@ionos.com>
2024-04-17 19:01:59 +0200

    fix europe

--- httpdocs/src/Views/Cockpit/Europe.php
+++ httpdocs/src/Views/Cockpit/Europe.php
@@ -1,20 +1,23 @@
 <section class="mainsection" id="cockpit_europe">
     <h2><?php  echo _('Europe');?></h2>
     <section>
-        <button onClick="component.add('europeangreens')"><?php  echo _('Add "Member of European Greens"');?></button>
         <button onClick="component.add('machenwaszaehlt')"><?php  echo _('Add "Machen, was zählt"');?></button>
-
     </section>
   
+    <section class="selected_only">  
+        <h3><?php  echo _('Size');?></h3>
+        <input type="range" min="50" max="600" value="200" step="5" class="slider" id="europe_size" oninput="europe.setSize(this)">      
+    </section>
     
     <?php require ("./src/Views/Components/ToFrontAndBack.php"); ?>
 </section>
 
 <script>
     class Europe{
-        addElement(input){
-            
+        setSize(input){
+            cockpit.target.getElementsByTagName('img')[0].style.width = input.value + 'px'
         }
+
     }
     const europe = new Europe();
 </script>
