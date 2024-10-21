b3c0309f0e3e19fe8dd7bdfb9971bd01bff35424
Thomas Rose <thomas.rose@ionos.com>
2024-10-21 13:07:19 +0200

    change menu

--- httpdocs/src/Views/Menu/Mint.php
+++ httpdocs/src/Views/Menu/Mint.php
@@ -1,32 +1,7 @@
 <nav>
-    <div class="dropdown" id="menu_mint">
-        <span><?php echo _( 'File' ); ?></span>
-        <div class="dropdown-content">
-            <button onClick="api.load('templates/' + config.starttemplate + '/start.html')">
-                <?php  echo _('New');?>
-            </button>
-            <button style="display:none" onClick="ui.setLang('de')">
-                <?php echo _( 'German' ); ?>
-            </button>
-            <button style="display:none" onClick="ui.setLang('en')">
-                <?php echo _( 'English' ); ?>
-            </button>
-            <?php if ( $this->env->user->is_admin() ) { ?>
-                <div class="divider"></div>
-                <a href="/index.php?c=frontend&m=log&scope=Info" class="menu-link">
-                    <?php  echo _('Log Info');?>
-                </a>
-                <a href="/index.php?c=frontend&m=log&scope=Sharepics" class="menu-link">
-                    <?php  echo _('Sharepics');?>
-                </a>
-                <a href="/index.php?c=frontend&m=log&scope=Log" class="menu-link">
-                    <?php  echo _('Logfile');?>
-                </a>
-            <?php } ?>
-            <div class="divider"></div>
-            <a href="index.php?c=frontend&m=create" class="menu-link">
-                <?php  echo _('Create');?>
-			</a>
+<div class="dropdown" id="menu_mint">
+        <span>MINT Sharepic-Generator</span>
+        <div class="dropdown-content"> 
             <a href="index.php?c=frontend&m=view&view=Imprint" class="menu-link">
                 <?php  echo _('Imprint');?>
 			</a>
@@ -39,19 +14,14 @@
         </div>
     </div>
 
-    <div class="dropdown" id="menu_de">
-        <span><?php echo _( 'Sharepic' ); ?></span>
-        <div class="dropdown-content">
-            <button onClick="api.load('templates/mint/start.html');">
-                <?php  echo _('Create Sharepic');?>
-            </button> 
-        </div>
-    </div>
-
     <?php if( isset( $show_my_sharepics ) && $show_my_sharepics === true) { ?>
     <div class="dropdown" id="menu_sharepics">
-        <span><?php echo _( 'My Sharepics' ); ?></span>
+        <span><?php echo _( 'File' ); ?></span>
         <div id="my-sharepics" class="dropdown-content">
+            <button onClick="api.load('templates/' + config.starttemplate + '/start.html')">
+                <?php  echo _('New');?>
+            </button>
+
             <button style="display:none" onClick="api.autosave();alert('Ok');">
                 <?php  echo _('Save');?>
             </button>
