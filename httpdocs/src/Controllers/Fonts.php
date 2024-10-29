82ee80d99310acedbae7557be122777ae1c7cf02
Thomas Rose <thomas.rose@ionos.com>
2024-10-29 19:15:25 +0100

    introduce new fonts

--- httpdocs/src/Controllers/Fonts.php
+++ httpdocs/src/Controllers/Fonts.php
@@ -23,8 +23,12 @@ class Fonts {
 		$fonts = array();
 		foreach ( $woffs as $woff ) {
 			$font    = pathinfo( $woff, PATHINFO_FILENAME );
-			$fonts[] = $font . '=' . $font;
+			$fonts[] = self::get_font_family_from_filename( $woff );
 		}
+
+		// in earlier version, the format was fontname=fontname.
+
+		$fonts = array_unique( $fonts );
 
 		return join( ';', $fonts );
 	}
@@ -38,20 +42,41 @@ class Fonts {
 		$pattern = "@font-face {
 	font-family: '%s';
 	src: url('../../.%s') format('woff2');
-	font-weight: 300;
+	font-weight: %s;
+	font-style: %s;
 }";
 		$css     = '';
 		foreach ( $woffs as $woff ) {
+			$parts = explode( '-', pathinfo( $woff, PATHINFO_FILENAME ) );
+
+			$family  = self::get_font_family_from_filename( $woff );
+			$charset = $parts[1];
+			$weight  = $parts[2];
+			$style   = $parts[3];
+
 			$css .= sprintf(
 				$pattern,
-				pathinfo( $woff, PATHINFO_FILENAME ),
-				$woff
+				$family,
+				$woff,
+				$weight,
+				$style
 			) . "\n";
 		}
 
 		$css .= '.mce-content-body { word-wrap: normal !important; }';
 
 		file_put_contents( self::CSS_FILE, $css ) || die( 'Could not write file' );
+	}
+
+	/**
+	 * Get the font family from the filename.
+	 *
+	 * @param string $filename The filename.
+	 * @return string The font family.
+	 */
+	private static function get_font_family_from_filename( $filename ) {
+		$parts = explode( '-', pathinfo( $filename, PATHINFO_FILENAME ) );
+		return ucFirst( $parts[0] );
 	}
 
 	/**
