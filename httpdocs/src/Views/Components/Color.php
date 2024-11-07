f1199f0243b4f8d3225da2c9a4e6a367aa896a3c
Thomas Rose <thomas.rose@ionos.com>
2024-11-07 14:24:12 +0100

    introduce color

--- httpdocs/src/Views/Components/Color.php
+++ httpdocs/src/Views/Components/Color.php
@@ -1,11 +1,16 @@
-<div class="color" id="<?php echo $color->id; ?>">
+<div class="color" id="<?php echo $color->id; ?>">   
+    <div class="palette">
+        <button class="no-button" data-blueprint="true" style="background-color: #ffffff" onClick="<?php echo $color->onclick; ?>(this.style.backgroundColor);"></button>
+    </div>
+    
     <input type="color" 
         value="<?php echo $color->value; ?>" 
         class="" 
         id="<?php echo $color->id; ?>" 
         oninput="<?php echo $color->oninput; ?>">
-    
-    <div class="palette">
-        <button class="no-button" data-blueprint="true" style="background-color: #ffffff" onClick="<?php echo $color->onclick; ?>(this.style.backgroundColor);"></button>
-    </div>
+
+    <button 
+        class="colorpicker" 
+        onclick="this.previousElementSibling.click();">
+    </button>
 </div>
