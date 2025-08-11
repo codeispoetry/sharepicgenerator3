ae578871e77a167aafaf44771e57dab5954f9ded
Thomas Rose <thomas.rose@ionos.com>
2025-08-11 09:19:34 +0200

    add matomo and changes

--- httpdocs/src/Views/Footer.php
+++ httpdocs/src/Views/Footer.php
@@ -1,4 +1,20 @@
-
-
+<?php
+if('mint' === $this->env->config->get( 'Main', 'tenant' )):?>
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.sharepic.mint-vernetzt.de/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+<?php endif; ?>
 </body>
 </html>
