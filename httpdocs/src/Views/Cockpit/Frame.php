9b268edfcb1dbbfdf6efee589d13b96a38416cda
Thomas Rose <thomas.rose@ionos.com>
2026-04-17 16:27:10 +0200

    changes nds

--- httpdocs/src/Views/Cockpit/Frame.php
+++ httpdocs/src/Views/Cockpit/Frame.php
@@ -4,18 +4,29 @@
     </h2>
 
     <section>
-        <h3><?php  echo _('Frame thickness');?></h3>
+        <h3>Rahmen</h3>
        
         <div style="display: flex; flex-direction: column; gap: 10px;">
             <label>
-                <input type="radio" name="frame_thickness" value="0" oninput="frame.setThickness(0)" checked> <?php  echo _('None');?>
+                <input type="radio" name="frame_thickness" value="0" oninput="frame.setThickness(0)" checked> ohne Rahmen
             </label>
             <label>
-                <input type="radio" name="frame_thickness" value="1" oninput="frame.setThickness(30)"> <?php  echo _('With frame');?>
+                <input type="radio" name="frame_thickness" value="1" oninput="frame.setThickness(30)"> mit Rahmen
             </label>
-
         </div> 
+    </section>
+
+    <section>
+        <h3>Machs's grün</h3>
        
+        <div style="display: flex; flex-direction: column; gap: 10px;">
+            <label>
+                <input type="radio" name="mg_settings" value="0" oninput="mg.set(0)" checked> verbergen
+            </label>
+            <label>
+                <input type="radio" name="mg_settings" value="1" oninput="mg.set(1)"> anzeigen
+            </label>
+        </div> 
     </section>
    
     
@@ -36,4 +47,15 @@
        
     }
     const frame = new Frame();
+
+    class MG{
+
+        set(value){
+            const element = document.getElementById("claim");
+            element.style.display = value === 1 ? 'block' : 'none';
+            undo.commit()
+        }
+
+    }
+    const mg = new MG();
 </script>
