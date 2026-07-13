8020c6035b9c9fd66a6ea3d9e5063f46dd9e5af7
Thomas Rose <thomas.rose@ionos.com>
2026-07-13 18:48:48 +0200

    changes

--- httpdocs/src/Views/Cockpit/Greenclassictext.php
+++ httpdocs/src/Views/Cockpit/Greenclassictext.php
@@ -12,29 +12,29 @@
         <div style="display: flex;font-size: 75%;margin-bottom: 0;justify-content:space-between;">
             <span><?php echo _('Line'); ?></span>
             <span class="no-nrw"><?php echo _('Colorset'); ?></span>
-            <span><?php echo _('Size'); ?></span>
-            <span class="no-nrw"><?php echo _('Indent'); ?></span>
+            <span class="no-lsa"><?php echo _('Size'); ?></span>
+            <span class="no-lsa"><?php echo _('Indent'); ?></span>
         </div>
         <?php for( $i = 1; $i <= 12; $i++ ) { ?>
         <div class="cockpit_greentext">
-            <?php echo $i; ?></strong>
-            <select class="linecolor" onChange="greenclassictext.setLineColorset(this, <?php echo $i; ?>)">
-                <option value="tannesand"><?php echo _('tanne/sand'); ?></option>
-                <option value="sandtanne"><?php echo _('sand/tanne'); ?></option>
+            <strong><?php echo $i; ?></strong>
+            <select id="linecolor_<?php echo $i; ?>" class="linecolor" onChange="greenclassictext.setLineColorset(this, <?php echo $i; ?>)">
+                <option class="no-lsa" value="tannesand"><?php echo _('tanne/sand'); ?></option>
+                <option class="no-lsa" value="sandtanne"><?php echo _('sand/tanne'); ?></option>
 
-                <option value="kleesand"><?php echo _('klee/sand'); ?></option>
-                <option value="sandklee"><?php echo _('sand/klee'); ?></option>
+                <option class="no-lsa" value="kleesand"><?php echo _('klee/sand'); ?></option>
+                <option class="no-lsa" value="sandklee"><?php echo _('sand/klee'); ?></option>
 
-                <option value="grastanne"><?php echo _('gras/tanne'); ?></option>
-                <option value="tannegras"><?php echo _('tanne/gras'); ?></option>
+                <option class="no-lsa" value="grastanne"><?php echo _('gras/tanne'); ?></option>
+                <option class="no-lsa" value="tannegras"><?php echo _('tanne/gras'); ?></option>
             </select>
-            <select class="linesize" onChange="greenclassictext.setLineSize(this, <?php echo $i; ?>)">
+            <select class="linesize no-lsa" onChange="greenclassictext.setLineSize(this, <?php echo $i; ?>)">
                 <option value="s"><?php echo _('S'); ?></option>
                 <option value="m"><?php echo _('M'); ?></option>
                 <option value="l"><?php echo _('L'); ?></option>
             </select>
         
-            <input type="range" class="lineindent" min="-300" max="300" value="0" oninput="greenclassictext.setLineIndent(this, <?php echo $i; ?>)">
+            <input type="range" class="lineindent no-lsa" min="-300" max="300" value="0" oninput="greenclassictext.setLineIndent(this, <?php echo $i; ?>)">
            
         </div>
         <?php } ?>
